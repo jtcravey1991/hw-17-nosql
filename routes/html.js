7ae0f737a725013192381dd31285b386212db66d
Jake <j.t.cravey1991@gmail.com>
2020-08-09 12:01:57 -0700

    created server.js

--- routes/html.js
+++ routes/html.js
@@ -3,12 +3,14 @@ const router = express.Router();
 
 router.get("/", function(req, res) {
     res.sendFile("index.html")
-})
+});
 
 router.get("/exercise", function(req, res) {
     res.sendFile("exercise.html");
-})
+});
 
 router.get("/stats", function(req, res) {
     res.sendFile("stats.html");
-})
+});
+
+module.exports = router;
